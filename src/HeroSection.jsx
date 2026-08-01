06bcdea1b00cddf870ae8b3be3542136fa5a83bb
Roshan Sah <roshan.sah0505@gmail.com>
2026-08-02 01:04:42 +0530

    Dotted Animation

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -21,7 +21,7 @@ function HeroSection() {
 
             {/* Text */}
             <p className="relative text-white text-[14px] font-medium tracking-[0.42px] leading-[10px] font-sans">
-                AI-Powered roshan
+                AI-Powered
             </p>
 
         </div>
