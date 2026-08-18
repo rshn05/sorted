6f307a10f9c52adb06882647446472f8f88d9820
Roshan Sah <roshan.sah0505@gmail.com>
2026-08-18 12:51:42 +0530

    Feature

--- src/FeatureSection.jsx
+++ src/FeatureSection.jsx
@@ -15,12 +15,12 @@ function FutureSection() {
 
                     {/* KEYWORD PLANNER */}
                     <div className="w-full bg-[#F3F3F1] rounded-[30px] overflow-hidden p-30 sm:p-5 md:p-6 lg:pt-[20px]">
-                        <h2 className="text-2xl ml-4 font-semibold sm:text-3xl md:text-4xl lg:text-[20px] text-black leading-tight tracking-[-1px] sm:mb-6 relative z-10">
+                        <h2 className="text-2xl ml-10 font-semibold sm:text-3xl md:text-4xl lg:text-[20px] text-black leading-tight tracking-[-1px] sm:mb-1 relative z-10">
                             Plan keywords that actually drive traffic</h2>
 
-                        <div className="w-full bg-white rounded-[24px] overflow-hidden px-3 sm:px-4 md:px-5 lg:px-4 pt-6 sm:pt-8 md:pt-9 pb-5 sm:pb-6">
+                        {/* <div className="w-full bg-white rounded-[24px] overflow-hidden px-3 sm:px-4 md:px-5 lg:px-4 pt-6 sm:pt-8 md:pt-9 pb-5 sm:pb-6"> */}
 
-                            <div className="w-full flex items-center justify-center translate-y-4 sm:translate-y-6">
+                            <div className="mb-3 w-full flex items-center justify-center translate-y-4 sm:translate-y-6">
 
                                 <img
                                     src={keywords1}
@@ -29,28 +29,28 @@ function FutureSection() {
                                 />
                             </div>
 
-                        </div>
+                        {/* </div> */}
 
                     </div>
 
                     {/* CONTENT CREATION */}
                     <div className="w-full bg-[#F3F3F1] rounded-[30px] overflow-hidden p-30 sm:p-5 md:p-6 lg:pt-[20px]">
-                        <h2 className="text-2xl ml-4 font-semibold sm:text-3xl md:text-4xl lg:text-[20px] text-black leading-tight tracking-[-1px] sm:mb-6 relative z-10">
+                        <h2 className="text-2xl ml-10 font-semibold sm:text-3xl md:text-4xl lg:text-[20px] text-black leading-tight tracking-[-1px] sm:mb-1 relative z-10">
                             Create content for every marketing channel</h2>
 
-                        <div className="w-full bg-white rounded-[24px] overflow-hidden px-3 sm:px-4 md:px-5 lg:px-4 pt-4 sm:pt-8 md:pt-9 pb-5 sm:pb-6">
+                        {/* <div className="w-full bg-white rounded-[24px] overflow-hidden px-3 sm:px-4 md:px-5 lg:px-4 pt-4 sm:pt-8 md:pt-9 pb-5 sm:pb-6"> */}
 
-                            <div className="w-full flex items-center justify-center translate-y-4 sm:translate-y-6">
+                            <div className="mb-3 w-full flex items-center justify-center translate-y-4 sm:translate-y-6">
 
                                 <img
                                     src={keywords2}
                                     alt="Content creation for every marketing channel"
-                                    className="w-[96%] sm:w-[94%] md:w-[92%] lg:w-[96%] h-auto object-contain"
+                                    className="w-[96%] sm:w-[94%] md:w-[92%] lg:w-[88.5%] h-auto object-contain"
                                 />
 
                             </div>
 
-                        </div>
+                        {/* </div> */}
 
                     </div>
 
